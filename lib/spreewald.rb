aba929c10e98a65bb8efd6ce00388e674de11b3b
Darius Roberts <darius.roberts@gmail.com>
2016-01-22 11:38:50 -0800

    quickfix. ignoring override priority, from cucumber_priority gem

--- lib/spreewald.rb
+++ lib/spreewald.rb
@@ -1,4 +1,10 @@
 # coding: UTF-8
+require 'cucumber'
+class Cucumber::Runtime::SupportCode
+   def step_match(*args)
+   	 #TODO: re-enable this when Cucumber-prioirty is back up-to-date with cucumber 2.0
+   end
+end
 
 require 'cucumber_priority'
 require "spreewald_support/version"
